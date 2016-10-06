8ae6e893112432c3aeebf1d074463af4e50a835b
pim <pim@zifzaf.com>
2016-10-06 19:54:48 +0300

    Working 'calculate MIC test'

--- test/test.js
+++ test/test.js
@@ -2,17 +2,42 @@
 
 var assert = require('assert');
 
-var key = Buffer.from('00112233445566778899AABBCCDDEEFF', 'hex');
 
 /*jshint -W016 */
-var payload = Buffer.from([64,160,198,0,20,128,1,0,2,76,47,251,70,206,90]);
+/*
+
+//var payload = Buffer.from([64, 160,198,0,20, 128, 1,0, 2, 76,47, 251,70,206,90]);
 var buffer = Buffer.from([76,47]);
 var size = 2;
 var address = payload.readUInt32LE(1);
 var dir = 0;
 var sequenceCounter = payload.readUInt16BE(6); //payload[6] + (payload[7]<<8);
 var mic = payload.readUInt32LE(payload.length - 4);
+*/
 /* jshint +W016 */
+
+var payload = Buffer.from('40F17DBE4900020001954378762B11FF0D', 'hex');  // payload => 'test'
+/*
+MHDR:       40
+FHDR:
+    DevAddr:    F17DBE49
+    Fctl:       00
+    Fcnt:       0200
+Fport:      01
+Data:       95437876
+MIC:        2B11FF0D
+*/
+var address = payload.readUInt32LE(1);
+var data = Buffer.from('95437876', 'hex');
+var size = 4;
+var sequenceCounter = payload.readUInt16LE(6); //payload[6] + (payload[7]<<8);
+var dir = 0;
+var mic = payload.readUInt32LE(payload.length - 4);
+
+var NwkSKey = Buffer.from('44024241ed4ce9a68c6a8bc055233fd3', 'hex');
+var AppSKey = Buffer.from('ec925802ae430ca77fd3dd73cb2cc588', 'hex');
+
+var AppKey = Buffer.from('00112233445566778899AABBCCDDEEFF', 'hex');
 
 
 var lorawanCrypto = require('../index');
@@ -22,56 +47,38 @@ describe('LoRaWANCrypto', function() {
     describe('computeMic', function() {
         
         it('should compute the MIC', function() {
-            assert.equal(lorawanCrypto.computeMic(buffer, size, key, address, dir, sequenceCounter), mic);
+            var checkMic = lorawanCrypto.computeMic(payload.slice(0, -4), 13, NwkSKey, address, dir, sequenceCounter); 
+            assert.equal(checkMic, mic);
         });
 
     });
 
     describe('payloadDecrypt()', function() {
-        it('should return <Buffer 12 34>', function() {
+        it('[decrypt] should return \'test\'', function() {
 
-            var decBuffer = lorawanCrypto.payloadDecrypt(
-                buffer, size,
-                key,
-                address,
-                dir,
-                sequenceCounter
-            );
-
-            // should be: hex 12 34
-            assert.equal(decBuffer.compare(Buffer.from([0x12, 0x34])), 0);
+            var decBuffer = lorawanCrypto.payloadDecrypt(data, size, AppSKey, address, dir, sequenceCounter);
+            assert.equal(decBuffer.toString('utf-8'), 'test');
         });
     });
 
     describe('payloadEncrypt', function() {
 
-        it('should return <Buffer 4c 2f>', function() {
+        it('[decrypt, encrypt] should return original encrypted buffer', function() {
 
             var encBuffer = lorawanCrypto.payloadEncrypt(
-                lorawanCrypto.payloadDecrypt(
-                    buffer, size,
-                    key,
-                    address,
-                    dir,
-                    sequenceCounter
-                ), size,
-                key,
-                address,
-                dir,
-                sequenceCounter
-            );
+                lorawanCrypto.payloadDecrypt(data, size, AppSKey, address, dir, sequenceCounter), 
+                size, AppSKey, address, dir, sequenceCounter);
 
             // should be: hex 4c 2f
-            assert.equal(encBuffer.compare(Buffer.from([0x4c, 0x2f])), 0);
+            assert.equal(encBuffer.compare(data), 0);
         });
     });
 
     describe('joinComputeMic', function() {
         
         it('should compute the Join MIC', function() {
-            var MIC = lorawanCrypto.joinComputeMic(buffer, size, key);
-            console.log(MIC);
-
+            var MIC = lorawanCrypto.joinComputeMic(data, size, AppKey);
+            MIC = 0;
             assert(true);
         });
 
@@ -82,13 +89,13 @@ describe('LoRaWANCrypto', function() {
         var joinRequestLength = 18;
 
         it('should encrypt the join message', function() {
-            console.log('ORIGINAL: ', joinRequest);
+            //console.log('ORIGINAL: ', joinRequest);
 
-            var encBuffer = lorawanCrypto.joinEncrypt(joinRequest, joinRequestLength, key);
-            console.log('LoRa ENC: ', encBuffer);
+            var encBuffer = lorawanCrypto.joinEncrypt(joinRequest, joinRequestLength, AppSKey);
+            //console.log('LoRa ENC: ', encBuffer);
 
-            var decBuffer = lorawanCrypto.joinDecrypt(encBuffer, encBuffer.length, key);
-            console.log('LoRa DEC: ', decBuffer);
+            var decBuffer = lorawanCrypto.joinDecrypt(encBuffer, encBuffer.length, AppSKey);
+            //console.log('LoRa DEC: ', decBuffer);
 
             assert(joinRequest.compare(decBuffer) === 0);
         });
